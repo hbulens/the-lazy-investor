092906f75d2024740132dbb99679c09334062a03
Hendrik Bulens <hendrik.bulens@gmail.com>
2020-05-03 14:20:52 +0200

    Call TestBed.resetTestingModule

--- src/client/src/app/layout/login/login.component.spec.ts
+++ src/client/src/app/layout/login/login.component.spec.ts
@@ -31,6 +31,10 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
+  afterEach(() => {
+    TestBed.resetTestingModule();
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
